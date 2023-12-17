b69ab9ec5beed1237738e4018600fbaabc9377e6
nanamitoshimasu <5nanami1@gmail.com>
2023-12-17 22:53:50 +0900

    add: 他のユーザーのマイページを追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   delete 'log_out', to: 'sessions#destroy', as: 'log_out'
+  get '/:username', to: 'user#show'
 
   resources :users, only: %i[show destroy]
   resources :sessions, only: %i[create destroy]
