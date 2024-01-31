5750e6f722f7bd5d4e0c95e0d4efbcad9892b797
nanamitoshimasu <5nanami1@gmail.com>
2024-02-01 02:35:12 +0900

    add: 問い合わせフォーム、プライバシーポリシー、利用規約

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   delete 'log_out', to: 'sessions#destroy', as: 'log_out'
-
+  get 'privacy_policy', to: 'tops#privacy_policy'
+  get 'terms_of_service', to: 'tops#terms_of_service'
+  
   resources :teams do
     resource :attendance, only: %i[create destroy], module: :teams
     member do
