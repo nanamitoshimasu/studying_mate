838ab07ea401dd5cc8405e1c53ca35456007d89e
nanamitoshimasu <5nanami1@gmail.com>
2024-02-08 01:03:09 +0900

    fix: carriewave2

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'yarumoribucket'
     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/yarumoribucket"
-    config.fog_public = true
+    config.fog_public = false 
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
