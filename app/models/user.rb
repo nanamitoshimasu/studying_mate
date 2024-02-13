ece8165161f743bab463b887f07012fe6c5f24f5
nanamitoshimasu <5nanami1@gmail.com>
2024-02-13 11:16:23 +0900

    fix:attend buttonとuser profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,25 +11,6 @@ class User < ApplicationRecord
   has_many :timers, dependent: :destroy
   has_many :break_times, dependent: :destroy
   
-  class << self
-    def find_or_create_from_auth_hash(auth_hash)
-      user_params = user_params_from_auth_hash(auth_hash)
-      find_or_create_by(email: user_params[:email]) do |user|
-        user.update(user_params)
-      end
-    end
-    
-    private
-
-    def user_params_from_auth_hash(auth_hash)
-      {
-        name: auth_hash.info.name,
-        email: auth_hash.info.email,
-        avatar: auth_hash.info.avatar,
-      }
-    end
-  end
-  
   def owner?(team)
     team.user_id == id
   end 
@@ -52,5 +33,24 @@ class User < ApplicationRecord
   
   def total_calculated_time
     timers.inject(0) { |sum, timer| sum + timer.calculated_time }
+  end
+
+  class << self
+    def find_or_create_from_auth_hash(auth_hash)
+      user_params = user_params_from_auth_hash(auth_hash)
+      find_or_create_by(email: user_params[:email]) do |user|
+        user.update(user_params)
+      end
+    end
+    
+    private
+
+    def user_params_from_auth_hash(auth_hash)
+      {
+        name: auth_hash.info.name,
+        email: auth_hash.info.email,
+        avatar: auth_hash.info.avatar,
+      }
+    end
   end
 end
