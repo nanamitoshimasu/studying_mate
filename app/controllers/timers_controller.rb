cf038f6c6b10443e0e20c6e54a4377d8273c393b
nanamitoshimasu <5nanami1@gmail.com>
2024-03-17 02:15:44 +0900

    add: timerとbreak_timeの編集コントローラ

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -25,6 +25,35 @@ class TimersController < ApplicationController
     end
   end
 
+  def edit_all_timestamps
+    @timer = Timer.find(params[:id])
+    @break_time = @timer.break_time.find(params[:id])
+  end
+
+  def change_all_timestamps
+    @timer = Timer.find(params[:id])
+    @break_time = @timer.break_time.find(params[:id])
+
+    # Timerの時刻を更新
+    start_time = Time.zone.local(*params[:timer].values_at('start_time(1i)', 'start_time(2i)', 'start_time(3i)', 'start_time(4i)', 'start_time(5i)'))
+    end_time = Time.zone.local(*params[:timer].values_at('end_time(1i)', 'end_time(2i)', 'end_time(3i)', 'end_time(4i)', 'end_time(5i)'))
+    @timer.start_time = start_time
+    @timer.end_time = end_time
+
+    # break_timeの時刻を更新
+    break_start_time = Time.zone.local(*params[:break_time].values_at('break_start_time(1i)', 'break_start_time(2i)', 'break_start_time(3i)', 'break_start_time(4i)', 'start_time(5i)'))
+    break_end_time = Time.zone.local(*params[:break_time].values_at('break_end_time(1i)', 'break_end_time(2i)', 'break_end_time(3i)', 'break_end_time(4i)', 'break_end_time(5i)'))
+    @break_time.break_start_time = break_start_time
+    @break_time.break_end_time = break_end_time
+
+    if @timer.save && @break_time.save
+      redirect_to change_all_timestamps_path, success: "できたよ"
+    else
+      flash.now[:error] = "できなかったよ" 
+      render :edit_all_timestamps, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_team
