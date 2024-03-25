e0615d6391235a1e5fee820b03bb95217496061f
nanamitoshimasu <5nanami1@gmail.com>
2024-03-25 16:55:13 +0900

    fix: チームタイマー表示、undefinded解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,12 @@ module ApplicationHelper
   def format_duration(seconds)
     return '00 00' unless seconds
 
-    hours = (seconds / 3600).floor
-    minutes = ((seconds % 3600) / 60).floor
-    "#{hours.to_s.rjust(2, '0')} #{minutes.to_s.rjust(2, '0')}"
+    hours = (seconds.abs / 3600).floor
+    minutes = ((seconds.abs % 3600) / 60).floor
+
+    formatted_hours = hours.to_s.rjust(2, '0')
+    formatted_minutes = minutes.to_s.rjust(2, '0')
+
+    formatted_time = "#{seconds < 0 ? '-' : ''}#{formatted_hours} #{formatted_minutes}"
   end
 end
