453244e57c6191b24b0cc00c63b527c4f8e57519
nanamitoshimasu <5nanami1@gmail.com>
2024-04-22 17:18:25 +0900

    fix: top page, usage responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,15 @@ module.exports = {
       fontFamily: {
         main: ["Kiwi Maru", "Kosugi Maru", "sans-serif"]
       }
-    }
+    },
+    screens: {
+      sm: '300px',
+      md: '669px',
+      lg: '1024px',
+      xl: '1280px'
+    },
   },  
+
   plugins: [require("daisyui")],
 
   daisyui: {
